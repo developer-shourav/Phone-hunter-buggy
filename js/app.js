d345ed5692d4188a097ef085fdcec4f571f71007
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-14 12:57:53 +0600

    Show all buttons bug solved

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const displayPhones = (phones, dataLimit) =>{
         showAll.classList.remove('d-none');
     }
     else{
-        showAll.classList.add('d-hidden');
+        showAll.classList.add('d-none');
     }
     
 
